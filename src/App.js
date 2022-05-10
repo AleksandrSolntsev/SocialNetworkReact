9775c26058f26295ad759cbaee69ee655825a756
Aleksandr Solntsev <san2008nas@gmail.com>
2022-05-10 18:28:31 +0300

    Repository

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import "./App.css";
 import HeaderContainer from "./components/Header/HeaderContainer.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
+
+//import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
@@ -15,7 +16,7 @@ import { connect } from "react-redux";
 import { compose } from "redux";
 import {withRouter} from "react-router-dom"
 import Preloader from "./components/common/Preloader/Preloader";
-
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 
 
 class App extends Component {
@@ -39,7 +40,14 @@ class App extends Component {
         <Route path='/profile/:userId?' 
           render ={ () => <ProfileContainer/>}/>  
         <Route path='/dialogs' 
-          render ={() => <DialogsContainer />}/> 
+          render ={() => {return (
+            <div>
+              <Suspense fallback={<div>Загрузка...</div>}>
+              <DialogsContainer />
+              </Suspense>
+            </div>
+          );}
+          }/> 
         <Route path='/news' 
           render={() =><News />}/>
         <Route path='/music' 
